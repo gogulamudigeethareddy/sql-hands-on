0d142756dfdf9d2fdb3b8a53c93f576ad8773af4
geethareddy <gogulamudigeethareddy@gmail.com>
2023-08-14 19:19:06 -0500

    sql syntax

--- postgres/AIRCRAFT_LOAD_DATA.sql
+++ postgres/AIRCRAFT_LOAD_DATA.sql
@@ -50,3 +50,16 @@ INSERT INTO public.aircraft_code(code, "aircraft_name")
 	VALUES ('AX','TransState');
 INSERT INTO public.aircraft_code(code, "aircraft_name")
 	VALUES ('UA','United Airlines, Inc.');
+INSERT INTO public.aircraft_code(code, "aircraft_name")
+	VALUES ('AU','August Airlines, Inc.');
+
+
+ALTER TABLE aircraft_code ADD COLUMN id SERIAL PRIMARY KEY;
+
+SELECT MIN(id), code, aircraft_name
+FROM public.aircraft_code
+GROUP BY id;
+
+DELETE FROM public.aircraft_code ac
+WHERE ac.id >= 27;
+
--- postgres/AIRCRAFT_QUERY.sql
+++ postgres/AIRCRAFT_QUERY.sql
@@ -1,1 +1,6 @@
-SELECT * FROM public.aircraft_code;
+SELECT  ac.id,
+        ac.code,
+        ac.aircraft_name
+FROM public.aircraft_code ac;
+
+SELECT COUNT(*)FROM public.aircraft_code;
